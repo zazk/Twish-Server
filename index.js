5d7f2743e2957820fcb71763d25a8de8ffbae2bc
Enrique Juan de Dios <juandedioz@gmail.com>
2018-08-11 20:48:55 -0500

    Added Search on Twitter Term

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
-var Twit = require('twit');
+const Twit = require('twit');
+const PORT = 3000;
+const MAX_RESULTS = 20;
 
-var T = new Twit({
-  consumer_key: '...',
-  consumer_secret: '...',
-  access_token: '...',
-  access_token_secret: '...',
+const T = new Twit({
+  consumer_key: process.env.API_KEY,
+  consumer_secret: process.env.API_SECRET_KEY,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
   timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
   strictSSL: true // optional - requires SSL certificates to be valid.
 });
 
-//
-//  search twitter for all tweets containing the word 'banana' since July 11, 2011
-//
-T.get('search/tweets', { q: 'banana since:2011-07-11', count: 100 }, function(
-  err,
-  data,
-  response
-) {
-  console.log(data);
+var express = require('express');
+var app = express();
+
+app.get('/hello/:term', function(req, res) {
+  getTweets(req.params.term, res);
 });
+
+app.listen(PORT);
+console.log(`Listening on port ${PORT}`);
+
+//  search twitter for all tweets containing the term.
+const getTweets = (term, res) => {
+  T.get(
+    'search/tweets',
+    { q: `${term} since:2011-07-11`, count: MAX_RESULTS },
+    (err, data, response) => {
+      console.warn(err, response);
+      if (data.statuses) {
+        res.json(parseData(data.statuses));
+      }
+    }
+  );
+};
+
+const parseData = tweets => {
+  return tweets.map(tweet => ({
+    text: tweet.text,
+    user: tweet.user.screen_name,
+    id: tweet.id_str
+  }));
+};
